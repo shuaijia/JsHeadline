72838dd21565496141ef402ddaf312ee56142a77
jiashuai <368419jiashuai>
2018-03-29 11:19:34 +0800

    jia:jsoup 集成

---
+++ base/src/main/java/com/jia/base/BaseWebViewActivity.java
@@ -0,0 +1,31 @@
+package com.jia.base;
+
+import android.os.Bundle;
+
+/**
+ * Description:
+ * Created by jia on 2018/3/29.
+ * 人之所以能，是相信能。
+ */
+
+public class BaseWebViewActivity extends BaseActivity {
+    @Override
+    protected void initActivityView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
