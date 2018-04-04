75f49a5cc26519380377fb6b049a2a7bb8e76050
jiashuai <368419jiashuai>
2018-04-04 10:51:42 +0800

    jia:新闻列表

--- module_news/src/main/java/com/e/jia/news/adapter/ImgNewsViewHolder.java
+++ module_news/src/main/java/com/e/jia/news/adapter/ImgNewsViewHolder.java
@@ -1,12 +1,17 @@
 package com.e.jia.news.adapter;
 
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.e.jia.news.R;
 import com.jia.libnet.bean.news.NewsBean;
+import com.jia.libui.CircularImage;
+import com.jia.libui.JsPopupWindow;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,6 +29,7 @@ public class ImgNewsViewHolder extends NewsBaseViewHolder {
     public TextView tv_abstract;// 简介
     public TextView tv_extra;
     public ImageView iv_dots;
+    private CircularImage iv_media;
 
     public ImgNewsViewHolder(View itemView) {
         super(itemView);
@@ -31,28 +37,59 @@ public class ImgNewsViewHolder extends NewsBaseViewHolder {
         tv_abstract = itemView.findViewById(R.id.tv_abstract);
         tv_extra = itemView.findViewById(R.id.tv_extra);
         iv_dots = itemView.findViewById(R.id.iv_dots);
-        iv_image=itemView.findViewById(R.id.iv_image);
+        iv_image = itemView.findViewById(R.id.iv_image);
+        iv_media = itemView.findViewById(R.id.iv_media);
     }
 
     @Override
     public void bindView(NewsBean.DataEntity data) {
         tv_title.setText(data.getTitle() + "");
         tv_abstract.setText(data.getAbstractX() + "");
-        StringBuffer extra=new StringBuffer();
-        extra.append(data.getMedia_name()+" ");
-        extra.append(data.getComments_count()+" ");
-        extra.append(stampToDate(data.getPublish_time()+""));
+        StringBuffer extra = new StringBuffer();
+        extra.append(data.getMedia_name() + " ");
+        extra.append(data.getComments_count() + " ");
+        extra.append(stampToDate(data.getPublish_time() + ""));
         tv_extra.setText(extra);
 
         Glide.with(itemView.getContext())
                 .load(data.getItem_source_url())
                 .into(iv_image);
+
+        if (data.getComments_count() == 0) {
+            extra.append("暂无评论 ");
+        } else {
+            extra.append(data.getComments_count() + "条评论 ");
+        }
+
+        extra.append(stampToDate(data.getPublish_time() + ""));
+        tv_extra.setText(extra);
+
+        if (!TextUtils.isEmpty(data.getMedia_avatar_url())) {
+            Glide.with(itemView.getContext())
+                    .load(data.getMedia_avatar_url())
+                    .into(iv_media);
+        } else {
+            iv_media.setVisibility(View.GONE);
+        }
+
+        iv_dots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new JsPopupWindow.Builder()
+                        .setContentViewId(R.layout.popup_news_list)
+                        .setWidth(100)
+                        .setHeight(50)
+                        .setContext(itemView.getContext())
+                        .build()
+                        .showAsLocation(iv_dots, Gravity.LEFT,0,0);
+            }
+        });
     }
 
     /*
 * 将时间戳转换为时间
 */
-    public static String stampToDate(String s){
+    public static String stampToDate(String s) {
         String res;
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
         long lt = new Long(s);
--- module_news/src/main/java/com/e/jia/news/adapter/TextNewsViewHolder.java
+++ module_news/src/main/java/com/e/jia/news/adapter/TextNewsViewHolder.java
@@ -1,11 +1,17 @@
 package com.e.jia.news.adapter;
 
+import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.e.jia.news.R;
 import com.jia.libnet.bean.news.NewsBean;
+import com.jia.libui.CircularImage;
+import com.jia.libui.JsPopupWindow;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,6 +28,7 @@ public class TextNewsViewHolder extends NewsBaseViewHolder {
     public TextView tv_abstract;// 简介
     public TextView tv_extra;
     public ImageView iv_dots;
+    private CircularImage iv_media;
 
     public TextNewsViewHolder(View itemView) {
         super(itemView);
@@ -29,23 +36,53 @@ public class TextNewsViewHolder extends NewsBaseViewHolder {
         tv_abstract = itemView.findViewById(R.id.tv_abstract);
         tv_extra = itemView.findViewById(R.id.tv_extra);
         iv_dots = itemView.findViewById(R.id.iv_dots);
+        iv_media = itemView.findViewById(R.id.iv_media);
     }
 
     @Override
     public void bindView(NewsBean.DataEntity data) {
         tv_title.setText(data.getTitle() + "");
         tv_abstract.setText(data.getAbstractX() + "");
-        StringBuffer extra=new StringBuffer();
-        extra.append(data.getMedia_name()+" ");
-        extra.append(data.getComments_count()+" ");
-        extra.append(stampToDate(data.getPublish_time()+""));
+        StringBuffer extra = new StringBuffer();
+        if (!TextUtils.isEmpty(data.getMedia_name())) extra.append(data.getMedia_name() + " ");
+        if (data.getComments_count() == 0) {
+            extra.append("暂无评论 ");
+        } else {
+            extra.append(data.getComments_count() + "条评论 ");
+        }
+
+        extra.append(stampToDate(data.getPublish_time() + ""));
         tv_extra.setText(extra);
+
+        if (!TextUtils.isEmpty(data.getMedia_avatar_url())) {
+            Glide.with(itemView.getContext())
+                    .load(data.getMedia_avatar_url())
+                    .into(iv_media);
+        } else {
+            iv_media.setVisibility(View.GONE);
+        }
+
+        iv_dots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new JsPopupWindow.Builder()
+                        .setContentViewId(R.layout.popup_news_list)
+                        .setContext(itemView.getContext())
+                        .setOutSideCancle(true)
+                        .setFouse(false)
+                        .setWidth((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, itemView.getContext().getResources().getDisplayMetrics()))
+                        .setHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, itemView.getContext().getResources().getDisplayMetrics()))
+                        .setAnimation(R.style.anim_pop)
+                        .build()
+                        .showAsLocation(iv_dots, Gravity.LEFT| Gravity.TOP, 100, 0);
+            }
+        });
     }
 
     /*
   * 将时间戳转换为时间
   */
-    public static String stampToDate(String s){
+    public static String stampToDate(String s) {
         String res;
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
         long lt = new Long(s);
