df01762411a5e6072d92eab56df41a15ab96dd48
jiashuai <368419jiashuai>
2018-04-04 12:24:44 +0800

    jia:新闻列表

--- module_picture/src/main/java/com/e/jia/picture/PictureFragment.java
+++ module_picture/src/main/java/com/e/jia/picture/PictureFragment.java
@@ -1,17 +1,29 @@
 package com.e.jia.picture;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewParent;
 
 import com.jia.base.BaseFragment;
 import com.jia.base.BasePresenter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jia on 2018/3/31.
  */
 
 public class PictureFragment extends BaseFragment {
+
+    private TabLayout mTabLayout;
+    private ViewPager vp_img_content;
+
+    private List<String> mTitles = new ArrayList<>();
+
     @Override
     protected View initFragmentView(LayoutInflater inflater) {
         view = inflater.inflate(R.layout.frag_picture,null);
@@ -20,12 +32,21 @@ public class PictureFragment extends BaseFragment {
 
     @Override
     protected void initFragmentChildView(View view) {
-
+        mTabLayout=view.findViewById(R.id.tl_img_titles);
+        vp_img_content=view.findViewById(R.id.vp_img_content);
     }
 
     @Override
     protected void initFragmentData(Bundle savedInstanceState) {
-
+        mTitles.add("全部");
+        mTitles.add("老照片");
+        mTitles.add("故事照片");
+        mTitles.add("摄影集");
+        for (String title : mTitles) {
+            TabLayout.Tab tab = mTabLayout.newTab(); //创建tab
+            tab.setText(title); //设置文字
+            mTabLayout.addTab(tab); //添加到tabLayout中
+        }
     }
 
     @Override
--- module_video/src/main/java/com/e/jia/video/VideoFragment.java
+++ module_video/src/main/java/com/e/jia/video/VideoFragment.java
@@ -1,17 +1,28 @@
 package com.e.jia.video;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.jia.base.BaseFragment;
 import com.jia.base.BasePresenter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jia on 2018/3/31.
  */
 
 public class VideoFragment extends BaseFragment {
+
+    private TabLayout mTabLayout;
+    private ViewPager vp_video_content;
+
+    private List<String> mTitles = new ArrayList<>();
+
     @Override
     protected View initFragmentView(LayoutInflater inflater) {
         view = inflater.inflate(R.layout.frag_video, null);
@@ -20,12 +31,25 @@ public class VideoFragment extends BaseFragment {
 
     @Override
     protected void initFragmentChildView(View view) {
-
+        mTabLayout=view.findViewById(R.id.tl_video_titles);
+        vp_video_content=view.findViewById(R.id.vp_video_content);
     }
 
     @Override
     protected void initFragmentData(Bundle savedInstanceState) {
-
+        mTitles.add("影视");
+        mTitles.add("娱乐");
+        mTitles.add("呆萌");
+        mTitles.add("游戏");
+        mTitles.add("原创");
+        mTitles.add("开眼");
+        mTitles.add("小品");
+        mTitles.add("生活");
+        for (String title : mTitles) {
+            TabLayout.Tab tab = mTabLayout.newTab(); //创建tab
+            tab.setText(title); //设置文字
+            mTabLayout.addTab(tab); //添加到tabLayout中
+        }
     }
 
     @Override
