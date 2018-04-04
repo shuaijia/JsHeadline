cd9570ff5c333231ea26a10a21525420522e5f80
jiashuai <368419jiashuai>
2018-04-04 18:14:13 +0800

    jia:新闻列表

--- module_news/src/main/java/com/e/jia/news/view/NewsFragment.java
+++ module_news/src/main/java/com/e/jia/news/view/NewsFragment.java
@@ -1,5 +1,6 @@
 package com.e.jia.news.view;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -7,9 +8,12 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.e.jia.news.adapter.NewsFragmentPagerAdapter;
 import com.e.jia.news.R;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetView;
 import com.jia.base.BaseFragment;
 import com.jia.base.BasePresenter;
 
@@ -20,10 +24,11 @@ import java.util.List;
  * Created by jia on 2018/3/31.
  */
 
-public class NewsFragment extends BaseFragment {
+public class NewsFragment extends BaseFragment implements View.OnClickListener{
 
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+    private ImageView iv_channel_add;
 
     private NewsFragmentPagerAdapter mNewsFragmentPagerAdapter;
     private List<String> mTitles = new ArrayList<>();
@@ -38,6 +43,25 @@ public class NewsFragment extends BaseFragment {
     protected void initFragmentChildView(View view) {
         mTabLayout = view.findViewById(R.id.tl_news_titles);
         mViewPager = view.findViewById(R.id.vp_news_content);
+        iv_channel_add = view.findViewById(R.id.iv_channel_add);
+
+        // 创建新功能引导
+         TapTargetView.showFor(getActivity(),
+                TapTarget.forView(view.findViewById(R.id.iv_channel_add), "点击这里，添加频道", "")
+                        .outerCircleColor(R.color.colorAccent)
+                        .outerCircleAlpha(0.96f)
+                        .targetCircleColor(R.color.white)
+                        .titleTextSize(20)
+                        .titleTextColor(R.color.colorAccent)
+                        .descriptionTextSize(10)
+                        .textColor(R.color.white)
+                        .textTypeface(Typeface.SANS_SERIF)
+                        .dimColor(R.color.white)
+                        .drawShadow(false)
+                        .cancelable(true)
+                        .tintTarget(false)
+                        .transparentTarget(false)
+                        .targetRadius(50));
     }
 
     @Override
@@ -73,4 +97,11 @@ public class NewsFragment extends BaseFragment {
         inflater.inflate(R.menu.menu_toolbar_news, menu);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.iv_channel_add) {
+
+
+        }
+    }
 }
