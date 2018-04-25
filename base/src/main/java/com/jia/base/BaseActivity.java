a4e7653e82d47c6ad58a44910597ee03839037b2
jiashuai <368419jiashuai>
2018-04-25 19:41:06 +0800

    应用内换肤

--- base/src/main/java/com/jia/base/BaseActivity.java
+++ base/src/main/java/com/jia/base/BaseActivity.java
@@ -1,7 +1,11 @@
 package com.jia.base;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.jia.base.annotation.BindEventBus;
 import com.jia.base.eventbus.EventBusUtils;
@@ -26,7 +30,7 @@ public abstract class BaseActivity<V, T extends BasePresenter<V>> extends RxAppC
         super.onCreate(savedInstanceState);
         SkinManager.getInstance().register(this);
         // 若使用BindEventBus注解，则绑定EventBus
-        if(this.getClass().isAnnotationPresent(BindEventBus.class)){
+        if (this.getClass().isAnnotationPresent(BindEventBus.class)) {
             EventBusUtils.register(this);
         }
 
@@ -68,12 +72,24 @@ public abstract class BaseActivity<V, T extends BasePresenter<V>> extends RxAppC
 
     protected abstract void initData();
 
+    public void setStatusColor(String theme) {
+        Window window = getWindow();
+        //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //设置状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(Color.parseColor(theme));
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         SkinManager.getInstance().unregister(this);
         // 若使用BindEventBus注解，则解绑定EventBus
-        if(this.getClass().isAnnotationPresent(BindEventBus.class)){
+        if (this.getClass().isAnnotationPresent(BindEventBus.class)) {
             EventBusUtils.unregister(this);
         }
         if (null != mPresenter) {
