b465248da53eb80a368dcf91fb5012a9e642f3dd
jiashuai <368419jiashuai>
2018-05-04 10:57:05 +0800

    搜索页

--- app/src/main/java/com/mangosteen/headline/SplashActivity.java
+++ app/src/main/java/com/mangosteen/headline/SplashActivity.java
@@ -1,12 +1,16 @@
 package com.mangosteen.headline;
 
+import android.animation.Animator;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.Window;
 import android.view.WindowManager;
+
+import yanzhikai.textpath.PathAnimatorListener;
+import yanzhikai.textpath.SyncTextPathView;
 
 
 /**
@@ -16,6 +20,9 @@ import android.view.WindowManager;
  */
 
 public class SplashActivity extends Activity {
+
+    private SyncTextPathView path_tv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +32,18 @@ public class SplashActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        path_tv = findViewById(R.id.path_tv);
+        path_tv.setTypeface(Typeface.SANS_SERIF);
+        path_tv.setAnimatorListener(new PathAnimatorListener(){
             @Override
-            public void run() {
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
             }
-        },2000);
+        });
+
+        //从无到显示
+        path_tv.startAnimation(0,1);
     }
 }
