278337a56b4cedf888fe3ab4d16fbf17f43c5a4f
jiashuai <368419jiashuai>
2018-06-05 15:05:59 +0800

    关于和设置

--- app/src/main/java/com/mangosteen/headline/view/AboutActivity.java
+++ app/src/main/java/com/mangosteen/headline/view/AboutActivity.java
@@ -1,22 +1,53 @@
 package com.mangosteen.headline.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
+import com.jia.base.BaseActivity;
+import com.jia.base.BasePresenter;
 import com.mangosteen.headline.R;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends BaseActivity {
 
     private Toolbar toolbar;
+    private TextView tv_author;
+    private TextView tv_version;
+    private TextView tv_github_url;
+    private TextView tv_points;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void initActivityView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    protected void initView() {
+        tv_author = findViewById(R.id.tv_author);
+        tv_version = findViewById(R.id.tv_version);
+        tv_github_url = findViewById(R.id.tv_github_url);
+        tv_points = findViewById(R.id.tv_points);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        // 左侧图标点击
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initData() {
+
     }
 }
--- app/src/main/java/com/mangosteen/headline/view/SettingActivity.java
+++ app/src/main/java/com/mangosteen/headline/view/SettingActivity.java
@@ -1,23 +1,44 @@
 package com.mangosteen.headline.view;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.jia.base.BaseActivity;
+import com.jia.base.BasePresenter;
 import com.mangosteen.headline.R;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends BaseActivity {
 
     private Toolbar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void initActivityView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_setting);
+    }
+
+    @Override
+    protected void initView() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        // 左侧图标点击
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initData() {
 
     }
 
